6a9561b27a6ed2d23dfa001e43aef9bd4892e366
murtuza <125211702+mominl@users.noreply.github.com>
2025-02-14 11:34:06 +0530

    Create python1.py

---
+++ python1.py
@@ -0,0 +1,8 @@
+def print_pattern(word):
+    """Prints the word in a vertical pattern, one letter per line."""
+
+    for i in range(len(word)):
+        print(word[i])
+
+# Example usage:
+print_pattern("ht eptyon")
